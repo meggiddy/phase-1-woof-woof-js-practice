5a26abbfb046a6e133930976054a90cb835e4f91
meggidy <mimamitchelle@gmail.com>
2022-12-07 12:39:08 +0300

    progress

--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
-document.addEventListener("DOMContentLoaded", () => {});
+document.addEventListener("DOMContentLoaded", () => {
+  createSpan();
+});
 const createSpan = async () => {
   const div = document.querySelector("#dog-bar");
-  const span = document.createElement("span");
-
+  const doginfo = document.querySelector("#dog-info");
+  const img = document.createElement("img");
   const dogs = await getDogs();
-  dogs.map((dog)=>{
-    span.textContent = dog.name
-    div.appendChild(span)
+  doginfo.append(img);
+  dogs.map((dog) => {
+    const span = document.createElement("span");
+    span.textContent = dog.name;
+    img.setAttribute("src", dog.image);
 
-  })
+    span.addEventListener("click", () => {});
+    div.append(span);
+  });
 };
 const getDogs = () => {
   return fetch("http://localhost:3000/pups")
     .then((response) => response.json())
     .then((data) => data);
+};
+const dispDog = () => {
+  getDogs;
 };
