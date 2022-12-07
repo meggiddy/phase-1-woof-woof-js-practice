c25a543ed1fa74e6f17d8bf4cd087c7874e9b2da
meggidy <mimamitchelle@gmail.com>
2022-12-07 13:56:53 +0300

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,26 +1,49 @@
-document.addEventListener("DOMContentLoaded", () => {
-  createSpan();
+document.addEventListener("DOMContentLoaded", async () => {
+  const dogs = await getDogs();
+  createSpan(dogs);
+  toggleGoodBad();
 });
-const createSpan = async () => {
+const createSpan = (dogs) => {
   const div = document.querySelector("#dog-bar");
   const doginfo = document.querySelector("#dog-info");
   const img = document.createElement("img");
-  const dogs = await getDogs();
+  const btn = document.createElement("button");
+
+  div.innerHTML = "";
+  doginfo.innerHTML = "";
+  doginfo.append(btn);
   doginfo.append(img);
+
   dogs.map((dog) => {
     const span = document.createElement("span");
     span.textContent = dog.name;
-    img.setAttribute("src", dog.image);
-
-    span.addEventListener("click", () => {});
+    span.addEventListener("click", () => {
+      img.setAttribute("src", dog.image);
+      btn.textContent = dog.isGoodDog ? "Good Dog!" : "Bad Dog!";
+    });
     div.append(span);
   });
 };
 const getDogs = () => {
-  return fetch("http://localhost:3000/pups")
-    .then((response) => response.json())
-    .then((data) => data);
+  return fetch("http://localhost:3000/pups").then((response) =>
+    response.json()
+  );
 };
-const dispDog = () => {
-  getDogs;
+
+let toggleValue = false;
+
+const toggleGoodBad = () => {
+  const toggle = document.querySelector("#good-dog-filter");
+  
+  toggle.addEventListener("click", async () => {
+    const dogs = await getDogs();
+    toggleValue = !toggleValue
+    if (toggleValue===true){
+        
+        return createSpan(dogs.filter((dog) => dog.isGoodDog));
+    } else {
+        return createSpan(dogs);
+    }
+    
+  });
 };
